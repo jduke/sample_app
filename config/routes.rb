e30006107d2fa934ae3262c3c3ba98c5f6a32cd3
Taras Duk <taras_duk@mail.ru>
2011-06-24 20:33:56 +0300

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
 	match '/contact', :to => 'pages#contact'
 	match '/about', :to => 'pages#about'
 	match '/help', :to => 'pages#help'
-
+	match '/signup', :to => 'users#new'
   get "pages/home"
 
   get "pages/contact"
